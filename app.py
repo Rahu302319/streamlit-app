15112eefd00980afb1f9f05771313f6702162efb
Eramsales <rahulcalicut1995@gmail.com>
2026-01-26 15:04:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ if user_input:
         "model": MODEL,
         "messages": st.session_state.messages,
         "temperature": 0.7,
-        "max_tokens": 2048,
+        "max_tokens": 1000048,
         "stream": True,
         "top_p": 1
     }
